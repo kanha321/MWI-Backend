fcf598110ab834c86f30af2875c88e7f32aa5d5b
kanha321 <shubh.sg321@gmail.com>
2025-03-05 17:29:25 +0530

    Initial BoilerPlate

---
+++ src/main/kotlin/com/kanhaji/mwibackend/MwiBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.kanhaji.mwibackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MwiBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MwiBackendApplication>(*args)
+}
